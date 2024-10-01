6aa991a6a15ce1db592f0767e7e15c45560e66b0
Dmitry Shumilin <55213131+dshumilin03@users.noreply.github.com>
2024-10-01 17:04:56 +0300

    added commentaries for circle.py functions

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    ```Принимает радиус, возвращает площадь круга```
     return math.pi * r * r
 
 
 def perimeter(r):
+        ```Принимает радиус, возвращает периметр круга```
     return 2 * math.pi * r
 
